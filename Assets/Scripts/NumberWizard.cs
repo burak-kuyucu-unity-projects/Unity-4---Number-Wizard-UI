e34a4d7ba762e4cc44cf2529a58fb1a70959af0a
Burak Kuyucu <kuyucuburakg@gmail.com>
2022-05-29 18:46:56 +0300

    first commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    void Start()
+    {
+        StartGame();    
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    private void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentScreneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScreneIndex + 1);
+    }
+
+    public void LoadStartScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
